4cb6c74f3b8bda0a5e3374d66689ee2ffe3aa88e
Moon613 <willzendgraft@gmail.com>
2025-06-29 21:57:36 -0400

    Add InverseLerp and Lerp to Utils

--- src/Utils.cs
+++ src/Utils.cs
@@ -4,7 +4,13 @@ namespace WorldCustomizer;
 
 public static class Utils {
     public static float LerpMap(float lowerRealVal, float upperRealVal, float lowerInverseLerpVal, float upperInverseLerpVal, float x) {
-        float newX = Math.Max(0, Math.Min(1, (x-lowerInverseLerpVal)/upperInverseLerpVal));
-        return lowerRealVal + upperRealVal * newX;
+        float newX = InverseLerp(x, lowerInverseLerpVal, upperInverseLerpVal);
+        return Lerp(lowerRealVal, upperRealVal, newX);
+    }
+    public static float InverseLerp(float x, float min, float max) {
+        return Math.Max(0, Math.Min(1, (x-min)/(max-min)));
+    }
+    public static float Lerp(float min, float max, float x) {
+        return Math.Max(min, Math.Min(max, min + max * x));
     }
 }
