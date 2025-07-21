ac07b2a54e110389ed8eb8f7ed6b0a50e3139371
Moon613 <willzendgraft@gmail.com>
2025-07-21 10:52:24 -0400

    OK zooming maybe

--- src/WorldRenderer.cs
+++ src/WorldRenderer.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Numerics;
+using System.Threading;
+using System.Threading.Tasks;
 using SDL2;
 
 #nullable enable
@@ -14,11 +16,13 @@ class WorldRenderer : GenericUIElement, IRenderable, IAmInteractable {
     IntPtr layer2Surface;
     IntPtr layer3Surface;
     IntPtr finalSurface;
+    Vector2 originalSize;
     private WorldData? WorldData { get { return GetParentWindow().parentProgram.currentWorld; }}
     public WorldRenderer(Vector2 position, Vector2 size, GenericUIElement parent) : base(position, size, parent) {
-        zoom = 2;
+        zoom = 1f;
         dragged = false;
         currentlyFocusedLayer = 0;
+        originalSize = size;
         layer1Surface = SDL.SDL_CreateRGBSurface(0, (int)size.X, (int)size.Y, 32, 0x000000FF, 0x0000FF00, 0x00FF0000, 0xFF000000);
         layer2Surface = SDL.SDL_CreateRGBSurface(0, (int)size.X, (int)size.Y, 32, 0x000000FF, 0x0000FF00, 0x00FF0000, 0xFF000000);
         layer3Surface = SDL.SDL_CreateRGBSurface(0, (int)size.X, (int)size.Y, 32, 0x000000FF, 0x0000FF00, 0x00FF0000, 0xFF000000);
@@ -37,14 +41,15 @@ class WorldRenderer : GenericUIElement, IRenderable, IAmInteractable {
         SDL.SDL_FillRect(finalSurface, (IntPtr)null, 0x00000000);
 
         if (WorldData != null) {
+            // Parallel.ForEach(WorldData.roomData, (room) => {RenderRoom(renderer, room);});
             foreach (RoomData room in WorldData.roomData) {
                 RenderRoom(renderer, room);
             }
         }
-        var rect = new SDL.SDL_Rect(){x=0, y=0, w=(int)size.X, h=(int)size.Y};
-        SDL.SDL_BlitSurface(layer3Surface, (IntPtr)null, finalSurface, ref rect);
-        SDL.SDL_BlitSurface(layer2Surface, (IntPtr)null, finalSurface, ref rect);
-        SDL.SDL_BlitSurface(layer1Surface, (IntPtr)null, finalSurface, ref rect);
+        var rect = new SDL.SDL_Rect(){x=0, y=0, w=(int)originalSize.X, h=(int)originalSize.Y};
+        SDL.SDL_BlitScaled(layer3Surface, (IntPtr)null, finalSurface, ref rect);
+        SDL.SDL_BlitScaled(layer2Surface, (IntPtr)null, finalSurface, ref rect);
+        SDL.SDL_BlitScaled(layer1Surface, (IntPtr)null, finalSurface, ref rect);
 
         var finalRect = new SDL.SDL_FRect(){x=0, y=0, w=size.X, h=size.Y};
         IntPtr texture = SDL.SDL_CreateTextureFromSurface(renderer, finalSurface);
@@ -60,23 +65,17 @@ class WorldRenderer : GenericUIElement, IRenderable, IAmInteractable {
         float scrollY = GetParentWindow().parentProgram.scrollY;
         
         // Checks that a zoom is actually happening and that it will not make the world dissappear.
-        if (!dragged && scrollY != 0 && scrollY*0.1f+zoom > 0.1f && scrollY*0.1f+zoom < 4) {
+        if (!dragged && scrollY != 0 && scrollY*0.1f+zoom > 1 && scrollY*0.1f+zoom < 4) {
             zoom += scrollY * 0.1f;
-            /***********
-            // I will do positioning later. 
-            // But based on what I'm thinking now, the best way to do it would be to render each room to a surface the size of the region,
-            // then resize the rectangle that is used to render that surface to the window.
-            ***********/
-            // Utils.DebugLog((Position-(size*0.5f)).Magnitude());
-            // if (scrollY > 0) {
-            //     Position += Vector2.Normalize(Position-(size*0.5f)) * 100/(2*zoom); //(Position-(size*0.5f)) * (0.05f-(float)Math.Pow((zoom-4)/18, 2));
-            // }
-            // else if (scrollY < 0) {
-            //     Position += (Position-(size*0.5f)) * -0.1f;
-            // }
-            // if (scrollY > 0) {
-            //     Position += (mousePos-(size*0.5f)) * (1f/(4*zoom));
-            // }
+            Utils.DebugLog((mousePos - (originalSize*0.5f))*0.05f);
+            if (scrollY > 0) {
+                Position -= (mousePos - (originalSize*0.5f))*0.05f;
+                size += new Vector2(64, 36);
+            }
+            else if (scrollY < 0) {
+                Position -= (mousePos - (originalSize*0.5f))*0.05f;
+                size -= new Vector2(64, 36);
+            }
         }
         
         if (GetParentWindow().IsFocused) {
@@ -96,7 +95,6 @@ class WorldRenderer : GenericUIElement, IRenderable, IAmInteractable {
         return parent.GetParentWindow();
     }
     void RenderRoom(IntPtr renderer, RoomData room) {
-        Utils.DebugLog($"Rendered room {room.name}");
         var r = new SDL.SDL_Rect(){x=(int)_position.X + (int)(room.devPosition.X*0.5f), y=(int)_position.Y + (int)(room.devPosition.Y*0.5f), w=(int)room.size.X, h=(int)room.size.Y};
         if (room.layer == 0) {
             SDL.SDL_BlitScaled(room.roomSurface, (IntPtr)null, layer1Surface, ref r);
